146ebb28e96320c92242d3f21225dd40190271d1
Jairo Prieto <jalprilon@gmail.com>
2014-04-29 22:58:05 -0500

    Archivo de formularios

---
+++ formularios.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+  <title>Formularios</title>
+  <meta charset="utf-8" />
+</head>
+<body>
+<form>
+  <div class="panel panel-default">
+    <div>
+    <label for="edad_1">Edad</label>
+    <input type="text" id="edad_1" name="edad_1" placeholder="Escriba su edad" />
+    </div>
+    <div>
+    <label for="sexo_1">Sexo</label>
+    <select id="sexo_1" name="sexo_1">
+      <option name="sexo_1" value="femenino">Femenino</option>
+      <option name="sexo_1" value="masculino">Masculino</option>
+    </select>
+    </div>
+    <div>
+    <label for="estCivil_1">Estado civil</label>
+    <select id="estCivil_1" name="estCivil_1">
+      <option name="estCivil_1" value="1">Soltero</option>
+      <option name="estCivil_1" value="2">Casado</option>
+      <option name="estCivil_1" value="3">Unión libre</option>
+      <option name="estCivil_1" value="4">Separado</option>
+      <option name="estCivil_1" value="5">Viudo</option>
+    </select>
+    </div>
+    <div>
+      <button>Enviar</button>
+    </div>
+  </div>
+</form>
+</body>
+</html>
